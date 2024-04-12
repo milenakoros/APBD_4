f3aa79c8b8885aaa777286680e9508da659e82e9
s28502 <s28502@pjwstk.edu.pl>
2024-04-13 00:12:28 +0200

    Animal and Visit classes added

---
+++ Klinika/Klinika/Animal.cs
@@ -0,0 +1,11 @@
+namespace Klinika;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Weight { get; set; }
+    public string FurColor { get; set; }
+    public List<Visit> Visits { get; set; } = new List<Visit>();
+}
